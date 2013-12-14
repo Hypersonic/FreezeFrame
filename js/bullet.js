5bca46187b39495d2c7ca801bbdfe4b863d34e13
justinkim <yulli@yulli.org>
2013-12-14 15:29:19 -0500

    Add partial detectCollision for bullets

--- js/bullet.js
+++ js/bullet.js
@@ -21,6 +21,28 @@ GAME.Bullet = (function() {
         bullet.y += yvel;
     }
 
+    function detectCollisions(bullet) {
+        for (ent : GAME.entities) {
+            if (GAME.Entity.dist(bullet, ent) < 1) {
+                if (ent.isPlayer) {
+                    GAME.end();
+                } else { // bullet hit an enemy
+                    GAME.yay();
+                    GAME.score++;
+                }
+            }
+        }
+
+        // look at 9 surrounding tiles
+        for (wall : walls) {
+            if (collision) {
+                theta = calculateThetaOut();
+                // undoStep(); // TODO: is this necessary?
+                bullet.path.push([bullet.x, bullet.y]);
+            }
+        }
+    }
+
 
     return {
         newBullet : newBullet,
