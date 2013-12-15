f6b8ad02a3ebd149912e16df4aba7b28015bb4a2
Zane Sterling <sterling.zane@gmail.com>
2013-12-15 11:06:16 -0500

    Beehavior

--- js/ai.js
+++ js/ai.js
@@ -2,10 +2,11 @@ GAME.AI = (function() {
 
 	var followAI = function(entity) {
 		var player = GAME.player,
-		    dx = player.x - entity.x,
-		    dy = player.y - entity.y;
+		    dx = player.x - entity.x + Math.random() * 20 - 10,
+		    dy = player.y - entity.y + Math.random() * 20 - 10;
 
 		entity.angle = Math.atan2(dy, dx);
+		entity.angle += Math.random();
 		GAME.Entity.move(entity, dx, dy);
 	}
 
